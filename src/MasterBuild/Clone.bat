dd09f41f751c6e8b1caf1a02ea7feaa351391be6
lightningviper <>
2018-02-02 18:42:55 -0700

    Fixing MasterBuild commands for use with Git

--- src/MasterBuild/Clone.bat
+++ src/MasterBuild/Clone.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 ECHO Cloning lightningviper/lightningstools repository...
-git clone "https://github.com/lightningviper/lightningstools" "%CD:~0,2%\lightningstools\"
+git clone "https://github.com/lightningviper/lightningstools" "%CD:~0,2%\lightningstools"
