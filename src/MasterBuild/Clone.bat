fd92ab3eb7480d8a6e8acc0b6b9a134c9c003cde
lightningviper <>
2018-03-03 00:05:32 -0700

    MasterBuild: fixed Clone.bat to use Git for Windows to perform Git clone of repository

--- src/MasterBuild/Clone.bat
+++ src/MasterBuild/Clone.bat
@@ -1,4 +1,3 @@
 @ECHO OFF
-
 ECHO Cloning lightningviper/lightningstools repository...
-git clone "https://github.com/lightningviper/lightningstools" "%CD:~0,2%\lightningstools"
+"%ProgramFiles%\Git\cmd\git.exe" clone "https://github.com/lightningviper/lightningstools" "%CD:~0,2%\lightningstools"
