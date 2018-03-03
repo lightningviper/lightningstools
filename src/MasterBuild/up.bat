71b0fdf4a3d2fe011c6cb58679e1151f74cd0d24
lightningviper <>
2018-03-02 22:55:19 -0700

    MasterBuild: fix up.bat to sequence installation of TortoiseGit ahead of rest of build machine prepare scripts

--- src/MasterBuild/up.bat
+++ src/MasterBuild/up.bat
@@ -3,13 +3,11 @@
 IF EXIST "%TEMP%\LightningsToolsInstall" RD /S /Q "%TEMP%\LightningsToolsInstall"
 MKDIR "%TEMP%\LightningsToolsInstall"
 
-IF NOT EXIST "%TEMP%\LightningsToolsInstall\Clone.bat" powershell Invoke-WebRequest -OutFile "%TEMP%\LightningsToolsInstall\Clone.bat" "https://raw.githubusercontent.com/lightningviper/lightningstools/master/src/MasterBuild/Clone.bat"
-IF ERRORLEVEL 1 GOTO END
 IF NOT EXIST "%TEMP%\LightningsToolsInstall\InstallTortoiseGit.bat" powershell Invoke-WebRequest -OutFile "%TEMP%\LightningsToolsInstall\InstallTortoiseGit.bat" "https://raw.githubusercontent.com/lightningviper/lightningstools/master/src/MasterBuild/InstallTortoiseGit.bat"
 IF ERRORLEVEL 1 GOTO END
 
 CD "%TEMP%\LightningsToolsInstall"
-CALL Clone.bat
+CALL InstallTortoiseGit.bat
 
 CD "%CD:~0,2%\lightningstools\src\MasterBuild"
 CALL PrepareBuildMachine.bat
