158caab18ee057a701c25b44c271a5b40da54eca
lightningviper <>
2018-08-23 11:21:43 -0700

    MasterBuild: Update Visual Studio Installer Projects add-in in machine setup scripts

--- src/MasterBuild/InstallVisualStudio2017InstallerProjectAddIn.bat
+++ src/MasterBuild/InstallVisualStudio2017InstallerProjectAddIn.bat
@@ -10,7 +10,7 @@ for /f "usebackq tokens=*" %%i in (`%MASTERBUILDDIR%vswhere.exe -latest -product
   set InstanceId=%%i
 )
 
-IF NOT EXIST "%MASTERBUILDDIR%InstallerProjects.vsix" bitsadmin /transfer VisualStudio2017InstallerProjectsAddIn /dynamic /download /priority HIGH https://visualstudioproductteam.gallerycdn.vsassets.io/extensions/visualstudioproductteam/microsoftvisualstudio2017installerprojects/0.8.4/1497369312093/247375/5/InstallerProjects.vsix "%MASTERBUILDDIR%InstallerProjects.vsix" 
+IF NOT EXIST "%MASTERBUILDDIR%InstallerProjects.vsix" bitsadmin /transfer VisualStudio2017InstallerProjectsAddIn /dynamic /download /priority HIGH https://visualstudioclient.gallerycdn.vsassets.io/extensions/visualstudioclient/microsoftvisualstudio2017installerprojects/0.8.8/1531988283007/InstallerProjects.vsix "%MASTERBUILDDIR%InstallerProjects.vsix" 
 
 
 IF ERRORLEVEL 1 GOTO END
