10c44639a7582fc8d95993ac2a722747d75e8248
lightningviper <>
2019-06-29 21:32:58 -0700

    MasterBuild: prevent multiple attempts to clone repository during machine prep

--- src/MasterBuild/Clone.bat
+++ src/MasterBuild/Clone.bat
@@ -1,3 +1,5 @@
 @ECHO OFF
+IF EXIST "%CD:~0,2%\lightningstools" GOTO END
 ECHO Cloning lightningviper/lightningstools repository...
 "%ProgramFiles%\Git\cmd\git.exe" clone "https://github.com/lightningviper/lightningstools" "%CD:~0,2%\lightningstools"
+:END
