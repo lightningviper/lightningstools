eb41941585db2898f36d80fb25fb9f5ee288638c
lightningviper <>
2021-05-13 16:27:18 -0700

    TeensyEWMU: set to CMDS mode by default in basicConfig.h since that's what most likely initial users have

--- src/SimLinkup/HardwareSupport/TeensyEWMU/Firmware/TeensyEWMU/basicConfig.h
+++ src/SimLinkup/HardwareSupport/TeensyEWMU/Firmware/TeensyEWMU/basicConfig.h
@@ -5,11 +5,10 @@
 #include "arduino.h"
 
 /* -------------- BASIC CONFIGURATION --------------- */
-const bool IS_CMDS =false; //set to TRUE if you are using this firmware with a CMDS panel
-const bool IS_EWMU =true; //set to TRUE if you are using this firmware with an EWMU panel
-const bool IS_EWPI =true; //set to TRUE if you are using this firmware with an EWPI panel
-const bool IS_EWMU_AND_EWPI_DAISY_CHAINED = true;
-const bool DISPLAY_TEST_PATTERN_AT_STARTUP=true;
+#define IS_CMDS =true; //set to TRUE if you are using this firmware with a CMDS panel
+#define IS_EWMU =false; //set to TRUE if you are using this firmware with an EWMU panel
+#define IS_EWPI =false; //set to TRUE if you are using this firmware with an EWPI panel
+#define DISPLAY_TEST_PATTERN_AT_STARTUP=false; //set to TRUE to turn all pixels on on all displays and light all lamps/LEDs at startup until a command is received from the PC host
 #define JOYSTICK_INTERFACE //comment out to not process joystick button returns
 /* -------------------------------------------------- */
 
